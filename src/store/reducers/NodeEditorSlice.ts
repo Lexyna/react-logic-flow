f0a817687d8b537c5d72d6db1635f63474b75130
Alexandra Thubeauville <alexandrathubeauville@Alexandras-MacBook-Air.local>
2022-04-28 12:05:43 +0200

    exported action and selector for nodeEditorStore

--- src/store/reducers/NodeEditorSlice.ts
+++ src/store/reducers/NodeEditorSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Connection } from "../../types/NodeEditorTypes";
 import { LogicNode } from "../../types/NodeTypes";
 
-export interface NodeEditorsStore {
+export interface NodeEditorsStoreState {
   [k: string]: NodeEditorStore;
 }
 export interface NodeEditorStore {
@@ -15,7 +15,7 @@ export interface NodeEditorStore {
 
 export const nodeEditorSlice = createSlice({
   name: "nodeEditors",
-  initialState: {} as NodeEditorsStore,
+  initialState: {} as NodeEditorsStoreState,
   reducers: {
     addNodeEditor: (state, action: PayloadAction<NodeEditorStore>) => {
       if (state[action.payload.id]) return;
@@ -25,4 +25,10 @@ export const nodeEditorSlice = createSlice({
   },
 });
 
+export const { addNodeEditor } = nodeEditorSlice.actions;
+
 export default nodeEditorSlice.reducer;
+
+export const selectNodeEditor = (id: string) => (state: NodeEditorsStoreState): NodeEditorStore => state[id];
+
+}

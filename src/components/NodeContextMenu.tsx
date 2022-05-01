3b05362184ac9aa15ad1717feb66abd7b9981fe0
lexyna <lexieverena@outlook.de>
2022-05-01 11:55:09 +0200

    Added working mouse/arrow select for contextMenu

--- src/components/NodeContextMenu.tsx
+++ src/components/NodeContextMenu.tsx
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import { MouseEvent, useState } from "react";
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
 import { ContextMenuProps } from "../types/ContextMenuTypes";
 import { LogicNode, ProtoNode } from "../types/NodeTypes";
 import "./../css/NodeContextMenu.css";
@@ -8,6 +8,9 @@ export const NodeContextMenu = (props: ContextMenuProps) => {
   let listId = 0;
 
   const [searchText, setSearchText] = useState<string>("");
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+  const [nodes, setNodes] = useState<ProtoNode[]>(props.config);
 
   const style = {
     top: `${props.y}px`,
@@ -15,16 +18,37 @@ export const NodeContextMenu = (props: ContextMenuProps) => {
   };
 
   const updateTextSearch = (s: string) => {
+    setSelectedIndex(0);
     setSearchText(s);
   };
 
-  const addLogicNode = (e: MouseEvent, protoNode: ProtoNode) => {
+  const onKeyDownHandler = (e: KeyboardEvent) => {
+    if (e.key === "ArrowUp")
+      setSelectedIndex((index) => {
+        if (index > 0) return index - 1;
+        return index;
+      });
+    if (e.key === "ArrowDown")
+      setSelectedIndex((index) => {
+        if (index < nodes.length - 1) return index + 1;
+        return index;
+      });
+    if (e.key === "Enter")
+      if (ref.current)
+        addLogicNode(
+          ref.current.getBoundingClientRect().x,
+          ref.current.getBoundingClientRect().y,
+          nodes[selectedIndex]
+        );
+  };
+
+  const addLogicNode = (x: number, y: number, protoNode: ProtoNode) => {
     const logicNode: LogicNode = {
       id: nanoid(),
       configId: protoNode.id,
       name: protoNode.name,
-      x: e.clientX / props.zoom - props.panning.offsetX,
-      y: e.clientY / props.zoom - props.panning.offsetY,
+      x: x / props.zoom - props.panning.offsetX,
+      y: y / props.zoom - props.panning.offsetY,
       inputs: protoNode.inputs,
       outputs: protoNode.outputs,
       forward: protoNode.forward,
@@ -32,10 +56,33 @@ export const NodeContextMenu = (props: ContextMenuProps) => {
     props.addNode(logicNode);
   };
 
+  useEffect(() => {
+    const newNodes: ProtoNode[] = [];
+
+    props.config.forEach((node) => {
+      if (
+        node.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+      )
+        newNodes.push(node);
+    });
+    setNodes(newNodes);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchText]);
+
+  useEffect(() => {
+    setSearchText("");
+  }, [props.show]);
+
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
     <div style={style} className="NodeContextMenuContainer">
       {props.show ? (
-        <div className="NodeContextMenu">
+        <div
+          ref={ref}
+          className="NodeContextMenu"
+          onKeyDown={(e) => onKeyDownHandler(e)}
+          tabIndex={0}>
           <div className="NodeContextMenuItem">
             <header>Search</header>
             <input
@@ -45,19 +92,18 @@ export const NodeContextMenu = (props: ContextMenuProps) => {
               onChange={(e) => updateTextSearch(e.target.value)}
             />
           </div>
-          {props.config.map((node) => {
-            if (
-              !node.name
-                .toLocaleLowerCase()
-                .includes(searchText.toLocaleLowerCase())
-            )
-              return null;
+          {nodes.map((node, index) => {
             listId++;
             return (
               <div
-                className="NodeContextMenuItem"
+                className={
+                  selectedIndex === index
+                    ? "NodeContextMenuItem selectedItem"
+                    : "NodeContextMenuItem"
+                }
                 key={listId}
-                onClick={(e) => addLogicNode(e, node)}>
+                onClick={(e) => addLogicNode(e.clientX, e.clientY, node)}
+                onMouseEnter={() => setSelectedIndex(index)}>
                 <header>{node.name}</header>
                 <span>{node.description}</span>
               </div>
